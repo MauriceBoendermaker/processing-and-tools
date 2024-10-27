8737bcc4b1134337603f78d7947826a60a1c9279
yassyass2 <yassinabde@outlook.com>
2024-10-27 22:18:12 +0100

    random endpoint toegevoegd

--- CargoHubV2/main.py
+++ CargoHubV2/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/status")
+async def stat():
+    return {"status": "originele data hebben en controllers maken"}
