6b591596fe5f9bf74707608cc70bc71af29964ba
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2024-11-11 21:08:40 +0100

    Created transfers schema file

---
+++ CargoHubV2/app/schemas/transfers_schema.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional, List, Dict
+
+
+class TransferBase(BaseModel):
+    reference: str
+    transfer_from: Optional[int] = None
+    transfer_to: int
+    items: List[Dict[str, int]]
+
+
+class TransferCreate(TransferBase):
+    pass
+
+
+class TransferUpdate(BaseModel):
+    reference: Optional[str] = None
+    transfer_from: Optional[int] = None
+    transfer_to: Optional[int] = None
+    items: Optional[List[Dict[str, int]]] = None
+    transfer_status: Optional[str] = None
+
+
+class Transfer(TransferBase):
+    id: int
+    transfer_status: str
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
