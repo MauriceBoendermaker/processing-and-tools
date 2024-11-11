c7470ff97f805bce8717871a920c47d250ca5a46
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2024-11-11 21:08:32 +0100

    Created transfers model file

---
+++ CargoHubV2/app/models/transfers_model.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, JSON, DateTime
+from CargoHubV2.app.database import Base
+from datetime import datetime
+
+
+class Transfer(Base):
+    __tablename__ = "transfers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    reference = Column(String, index=True)
+    transfer_from = Column(Integer, nullable=True)
+    transfer_to = Column(Integer, nullable=False)
+    transfer_status = Column(String, default="Scheduled", index=True)
+    created_at = Column(DateTime, default=datetime.utcnow, index=True)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, index=True)
+    items = Column(JSON, index=True)
