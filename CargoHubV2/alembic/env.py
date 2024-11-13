2bb9268fabaa649aaf2ced51e70c8033a489b15b
Motje <Mohamaadflaha2014@gmail.com>
2024-11-13 18:13:18 +0100

    New migrations ran

--- CargoHubV2/alembic/env.py
+++ CargoHubV2/alembic/env.py
@@ -1,14 +1,18 @@
+import sys
+from os.path import abspath, dirname
 from logging.config import fileConfig
 
-from app.database import Base
+# Add the root directory of the project to the path
+sys.path.append(abspath(dirname(dirname(__file__))))
+
+from app.models import Base, Item, ItemGroup, ItemType, ItemLine, Warehouse, Location, Transfer
 from app.models import (
     items_model, items_groups_model, items_types_model, items_lines_model,
-    warehouses_model, locations_model, transfers_model  # Import all models/ !!!!!ANY NEW MODELS NEED TO BE ADDED HERE!!!!!!
+    warehouses_model, locations_model, transfers_model  # Import all models
 )
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -22,28 +26,10 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
-
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
+    """Run migrations in 'offline' mode."""
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -55,14 +41,8 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
-
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
+    """Run migrations in 'online' mode."""
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
@@ -70,13 +50,10 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
--- CargoHubV2/app/models/__init__.py
+++ CargoHubV2/app/models/__init__.py
@@ -1,4 +1,11 @@
-from .items_model import Item  # Adjust this import if your model classes have different names
+# app/models/__init__.py
+
+from app.database import Base  # Import Base from database module
+from .items_model import Item
 from .items_groups_model import ItemGroup
 from .items_types_model import ItemType
 from .items_lines_model import ItemLine
+from .warehouses_model import Warehouse
+from .locations_model import Location
+from .transfers_model import Transfer
+# Import other models as needed
