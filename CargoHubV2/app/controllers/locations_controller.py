89802a8e042baf28ea3239596ed830e5287538c8
Thijs <1051036@hr.nl>
2024-11-22 14:03:54 +0100

    Fixed: Offset & Limit

--- CargoHubV2/app/controllers/locations_controller.py
+++ CargoHubV2/app/controllers/locations_controller.py
@@ -27,7 +27,7 @@ def get_location_by_id(id: int, db: Session = Depends(get_db)):
 
 @router.get("/warehouse/{warehouse_id}", response_model=List[locations_schema.Location])
 def get_locations_by_warehouse_id(warehouse_id: int, db: Session = Depends(get_db), offset = 0, limit = 100):
-    locations = locations_service.get_locations_by_warehouse_id(db, warehouse_id)
+    locations = locations_service.get_locations_by_warehouse_id(db, warehouse_id, offset=offset, limit=limit)
     if not locations:
         raise HTTPException(status_code=404, detail="Location warehouse not found")
     return locations
