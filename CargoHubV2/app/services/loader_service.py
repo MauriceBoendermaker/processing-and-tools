cde08817eb1dd34b87b0e8bd135df5bd4b45575e
yassyass2 <yassinabde@outlook.com>
2024-11-22 12:14:35 +0100

    Loader is nu voor alles in 1 keer

--- CargoHubV2/app/services/loader_service.py
+++ CargoHubV2/app/services/loader_service.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from CargoHubV2.app.models import (items_model, item_groups_model, item_lines_model,
                                    item_types_model, warehouses_model, transfers_model,
-                                   locations_model, suppliers_model)
+                                   locations_model, suppliers_model, shipments_model, 
+                                   clients_model, inventories_model)
 import os
 import json
 from tqdm import tqdm
@@ -18,29 +19,34 @@ model_mapping = {
         "warehouses.json": warehouses_model.Warehouse,
         "transfers.json": transfers_model.Transfer,
         "locations.json": locations_model.Location,
-        "suppliers.json": suppliers_model.Supplier
+        "suppliers.json": suppliers_model.Supplier,
+        "shipments.json": shipments_model.Shipment,
+        "clients.json": clients_model.Client,
+        "inventories.json": inventories_model.Inventory
     }
 
 
 def load(path: str, db: Session):
     base_dir = os.path.abspath(os.path.join(__file__, "../../../../.."))
 
-    # path naar json
-    json_file_path = os.path.join(base_dir, "data", path)
+    for file in model_mapping:
 
-    # file openen en loaden
-    with open(json_file_path, 'r') as json_file:
-        data = json.load(json_file)
+        # path naar json
+        json_file_path = os.path.join(base_dir, "data", file)
 
-    for i in tqdm(data):
-        if (path == "transfers.json" or path == "shipments.json" or path == "orders.json"):
-            i["created_at"] = datetime.strptime(i["created_at"], "%Y-%m-%dT%H:%M:%SZ")
-            i["updated_at"] = datetime.strptime(i["updated_at"], "%Y-%m-%dT%H:%M:%SZ")
-        else:
-            i["created_at"] = datetime.strptime(i["created_at"], "%Y-%m-%d %H:%M:%S")
-            i["updated_at"] = datetime.now()
-        obj = model_mapping[path](**i)
-        db.add(obj)
+        # file openen en loaden
+        with open(json_file_path, 'r') as json_file:
+            data = json.load(json_file)
+
+        for i in tqdm(data):
+            if (file == "transfers.json" or file == "shipments.json" or file == "orders.json"):
+                i["created_at"] = datetime.strptime(i["created_at"], "%Y-%m-%dT%H:%M:%SZ")
+                i["updated_at"] = datetime.strptime(i["updated_at"], "%Y-%m-%dT%H:%M:%SZ")
+            else:
+                i["created_at"] = datetime.strptime(i["created_at"], "%Y-%m-%d %H:%M:%S")
+                i["updated_at"] = datetime.now()
+            obj = model_mapping[file](**i)
+            db.add(obj)
 
     try:
         db.commit()
