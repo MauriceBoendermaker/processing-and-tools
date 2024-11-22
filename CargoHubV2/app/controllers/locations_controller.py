d2df8ec2abf235a5ab79c321ba398b301cf25257
Thijs <1051036@hr.nl>
2024-11-22 14:11:24 +0100

    Added: API protection for Locations

--- CargoHubV2/app/controllers/locations_controller.py
+++ CargoHubV2/app/controllers/locations_controller.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Header
 from sqlalchemy.orm import Session
 from CargoHubV2.app.services import locations_service
 from CargoHubV2.app.schemas import locations_schema
 from CargoHubV2.app.database import get_db
+from CargoHubV2.app.services.api_keys_service import validate_api_key
 from typing import List
 
 
@@ -13,12 +14,14 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[locations_schema.Location])
-def get_all_locations(db: Session = Depends(get_db), offset = 0, limit = 100):
+def get_all_locations(db: Session = Depends(get_db), offset = 0, limit = 100, api_key: str = Header(...)):
+    validate_api_key("view", api_key, db)
     return locations_service.get_all_locations(db, offset=offset, limit=limit)
 
 
 @router.get("/{id}", response_model=locations_schema.Location)
-def get_location_by_id(id: int, db: Session = Depends(get_db)):
+def get_location_by_id(id: int, db: Session = Depends(get_db), api_key: str = Header(...)):
+    validate_api_key("view", api_key, db)
     location = locations_service.get_location_by_id(db, id)
     if not location:
         raise HTTPException(status_code=404, detail="Location id not found")
@@ -26,7 +29,8 @@ def get_location_by_id(id: int, db: Session = Depends(get_db)):
 
 
 @router.get("/warehouse/{warehouse_id}", response_model=List[locations_schema.Location])
-def get_locations_by_warehouse_id(warehouse_id: int, db: Session = Depends(get_db), offset = 0, limit = 100):
+def get_locations_by_warehouse_id(warehouse_id: int, db: Session = Depends(get_db), offset = 0, limit = 100, api_key: str = Header(...)):
+    validate_api_key("view", api_key, db)
     locations = locations_service.get_locations_by_warehouse_id(db, warehouse_id, offset=offset, limit=limit)
     if not locations:
         raise HTTPException(status_code=404, detail="Location warehouse not found")
@@ -34,19 +38,22 @@ def get_locations_by_warehouse_id(warehouse_id: int, db: Session = Depends(get_d
 
 
 @router.post("/")
-def create_location(location: locations_schema.LocationCreate, db: Session = Depends(get_db)):
+def create_location(location: locations_schema.LocationCreate, db: Session = Depends(get_db), api_key: str = Header(...)):
+    validate_api_key("create", api_key, db)
     db_location = locations_service.create_location(db, location)
     return db_location
 
 
 @router.put("/{id}")
-def update_location(id: int, location_data: locations_schema.LocationUpdate, db: Session = Depends(get_db)):
+def update_location(id: int, location_data: locations_schema.LocationUpdate, db: Session = Depends(get_db), api_key: str = Header(...)):
+    validate_api_key("edit", api_key, db)
     updated_location = locations_service.update_location(db, id, location_data)
     return updated_location
 
 
 @router.delete("/{id}")
-def delete_location(id: int, db: Session = Depends(get_db)):
+def delete_location(id: int, db: Session = Depends(get_db), api_key: str = Header(...)):
+    validate_api_key("delete", api_key, db)
     result = locations_service.delete_location(db, id)
     if result:
         return {"detail": "Location deleted"}
