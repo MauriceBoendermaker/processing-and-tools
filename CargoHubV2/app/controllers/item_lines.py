d4a253f6503010aecd0d13846fdee839f9ace3ea
Motje3 <mohamaadflaha2014@gmail.com>
2024-12-03 15:17:36 +0100

    removing comments

--- CargoHubV2/app/controllers/item_lines.py
+++ CargoHubV2/app/controllers/item_lines.py
@@ -30,7 +30,7 @@ def get_item_lines(
     offset: int = 0,
     limit: int = 100,
     sort_by: Optional[str] = "id",  
-    order: Optional[str] = "asc",  # Default order is ascending
+    order: Optional[str] = "asc",  
     db: Session = Depends(get_db),
     api_key: str = Header(...),  # API key required
 ):
