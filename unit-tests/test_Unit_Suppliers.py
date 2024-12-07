84bfd78693352b9d566c02fb544bb1ff4462d1a0
Motje <Mohamaadflaha2014@gmail.com>
2024-12-07 19:53:18 +0100

    suppliers test

--- unit-tests/test_Unit_Suppliers.py
+++ unit-tests/test_Unit_Suppliers.py
@@ -70,23 +70,22 @@ def test_get_supplier_not_found():
 
 
 def test_get_all_suppliers():
-    
-    # Mock the database session
     db = MagicMock()
-    
-    # Mock the query chain
     mock_query = db.query.return_value
     mock_query.offset.return_value = mock_query
     mock_query.limit.return_value = mock_query
     mock_query.all.return_value = [Supplier(**supplier_sample_data)]
-    
-    # Call the function
-    results = get_all_suppliers(db, offset=0, limit=10)
-    
-    # Assertions
-    assert len(results) == 1
-    db.query.assert_called_once()
-    mock_query.all.assert_called_once()
+
+    with patch("CargoHubV2.app.services.suppliers_service.apply_sorting", return_value=mock_query) as mock_sorting:
+        results = get_all_suppliers(db, offset=0, limit=10, sort_by="id", order="asc")
+
+        mock_sorting.assert_called_once_with(mock_query, Supplier, "id", "asc")
+        db.query.assert_called_once_with(Supplier)
+        mock_query.offset.assert_called_once_with(0)
+        mock_query.limit.assert_called_once_with(10)
+        mock_query.all.assert_called_once()
+
+        assert len(results) == 1
 
 
 
