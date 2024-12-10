8f74babde176a6cf75f6b3e31ff020b61ee85830
Thijs <1051036@hr.nl>
2024-12-10 15:52:37 +0100

    Updated controller and service to update order_id

--- CargoHubV2/app/services/shipments_service.py
+++ CargoHubV2/app/services/shipments_service.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from CargoHubV2.app.models.shipments_model import Shipment
 from CargoHubV2.app.models.orders_model import Order
-from CargoHubV2.app.schemas.shipments_schema import ShipmentCreate, ShipmentUpdate
+from CargoHubV2.app.schemas.shipments_schema import ShipmentCreate, ShipmentUpdate, ShipmentOrderUpdate
 from fastapi import HTTPException, status
 from datetime import datetime
 
@@ -51,6 +51,30 @@ def get_all_shipments(db: Session):
         )
 
 
+
+
+def delete_shipment(db: Session, shipment_id: int):
+    try:
+        shipment = db.query(Shipment).filter(Shipment.id == shipment_id).first()
+        if not shipment:
+            raise HTTPException(status_code=404, detail="Shipment not found")
+        db.delete(shipment)
+        db.commit()
+    except SQLAlchemyError:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred while deleting the shipment."
+        )
+    return {"detail": "Shipment deleted"}
+
+
+def get_orders_by_shipment_id(db: Session, shipment_id:int):
+    orders = db.query(Order).filter(Order.shipment_id == shipment_id).all()
+    if not orders:
+        raise HTTPException(status_code=404, detail="No orders found")
+    return orders
+
 def update_shipment(db: Session, shipment_id: int, shipment_data: ShipmentUpdate):
     try:
         shipment = db.query(Shipment).filter(Shipment.id == shipment_id).first()
@@ -77,24 +101,21 @@ def update_shipment(db: Session, shipment_id: int, shipment_data: ShipmentUpdate
     return shipment
 
 
-def delete_shipment(db: Session, shipment_id: int):
+def update_orders_in_shipment(db: Session, shipment_id:int, shipment_data: ShipmentOrderUpdate):
     try:
         shipment = db.query(Shipment).filter(Shipment.id == shipment_id).first()
         if not shipment:
-            raise HTTPException(status_code=404, detail="Shipment not found")
-        db.delete(shipment)
-        db.commit()
+            raise HTTPException(status_code=404, detail="No Shipment found")
+        update_data =shipment_data.model_dump(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(shipment,key,value)
+            shipment.updated_at = datetime.now()
+            db.commit()
+            db.refresh(shipment)
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="An integrity error occurred while updating the shipment.")
     except SQLAlchemyError:
         db.rollback()
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="An error occurred while deleting the shipment."
-        )
-    return {"detail": "Shipment deleted"}
-
-
-def get_orders_by_shipment_id(db: Session, shipment_id:int):
-    orders = db.query(Order).filter(Order.shipment_id == shipment_id).all()
-    if not orders:
-        raise HTTPException(status_code=404, detail="No orders found")
-    return orders
+        raise HTTPException(status_code=500, detail="An error occured while updating the shipment.")
+    return shipment
