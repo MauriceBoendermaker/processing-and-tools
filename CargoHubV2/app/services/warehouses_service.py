c1eab5739a502c7d6baf40e263d7f0febad95824
Motje3 <mohamaadflaha2014@gmail.com>
2024-12-17 13:29:28 +0100

    warehouse

--- CargoHubV2/app/services/warehouses_service.py
+++ CargoHubV2/app/services/warehouses_service.py
@@ -67,21 +67,24 @@ def create_warehouse(db: Session, warehouse: dict):
 
 
 def delete_warehouse(db: Session, code: str):
-    to_del = db.query(Warehouse).filter(Warehouse.code == code).first()
+    to_del = db.query(Warehouse).filter(
+        Warehouse.code == code, Warehouse.is_deleted == False
+    ).first()
     if not to_del:
         raise HTTPException(status_code=404, detail="Warehouse not found")
 
     try:
-        db.delete(to_del)
+        to_del.is_deleted = True  # Soft delete by updating the flag
         db.commit()
     except SQLAlchemyError:
         db.rollback()
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="An error occured while deleting the warehouse"
+            detail="An error occurred while deleting the warehouse"
         )
 
-    return True
+    return {"detail": "Warehouse soft deleted"}
+
 
 
 def update_warehouse(db: Session, code: str, warehouse_data: dict) -> WarehouseResponse:
