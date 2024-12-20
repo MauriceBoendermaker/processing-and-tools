75b66380b5cb937afa7b34ad3cca3cd96fbd7bc7
yassyass2 <yassinabde@outlook.com>
2024-12-20 16:09:49 +0100

    change valueerror to HTTPException in sorting

--- CargoHubV2/app/services/sorting_service.py
+++ CargoHubV2/app/services/sorting_service.py
@@ -1,12 +1,14 @@
 from sqlalchemy.orm import Query
 from sqlalchemy.exc import SQLAlchemyError
+from fastapi import HTTPException
+
 
 def apply_sorting(query: Query, model, sort_by: str = "uid", order: str = "asc") -> Query:
     #You can change the sortby from id to anything in the database or anything the model has as a prop
     try:
         # Validate that the column exists in the model
         if not hasattr(model, sort_by):
-            raise ValueError(f"Invalid sort attribute: {sort_by}")
+            raise HTTPException(400, f"Invalid sort attribute: {sort_by}")
 
         # Get the column from the model
         column = getattr(model, sort_by)
@@ -16,4 +18,4 @@ def apply_sorting(query: Query, model, sort_by: str = "uid", order: str = "asc")
             return query.order_by(column.desc())
         return query.order_by(column.asc())
     except SQLAlchemyError as e:
-        raise ValueError(f"Error applying sorting: {str(e)}")
+        raise HTTPException(500, f"Error applying sorting: {str(e)}")
