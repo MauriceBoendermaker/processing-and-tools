7189aa14e4e924b34ba44597c22d512be1faaec0
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-01-05 12:37:19 +0100

    Defined top 10 list of shipping hazard (classifications)

--- CargoHubV2/populate_hazard_classifications.py
+++ CargoHubV2/populate_hazard_classifications.py
@@ -3,6 +3,21 @@ from sqlalchemy import create_engine, MetaData, Table, select
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///Cargo_Database.db"
+
+# Gevaar classes
+hazard_classes = [
+    "Flammable",
+    "Explosive",
+    "Toxic",
+    "Non-hazardous",
+    "Corrosive",
+    "Radioactive",
+    "Oxidizing",
+    "Infectious",
+    "Combustible",
+    "Compressed gas"
+]
+
 # Set up voor SQLAlchemy
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
